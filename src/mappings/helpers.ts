25a960813f51dfcf59eb83413790fb41ba165b73
Captain Cryptoo <captaincryptoooo@gmail.com>
2022-04-19 16:17:49 +0500

    new commit

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -8,7 +8,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xAcA5aFa30910Da31bDa70Aa3496eeEAE0D1BfE06'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
